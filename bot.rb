1b3f69b21bd2a845de587d8858074718b72552ed
DarkyOMG <darkyomg@gmx.de>
2019-10-13 23:37:27 +0200

    First Build

--- bot.rb
+++ bot.rb
@@ -1,1 +1,9 @@
-puts 4
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: 'NjMzMDAyODQ5NjM0MjIyMTM0.XaOT0g.m9XN0lG93M6peNpJnCScpPWpyEc'
+
+bot.message(start_with: '§') do |event|
+  event.respond 'Pong!'
+end
+
+bot.run
--- testfile.rb
+++
@@ -1,1 +0,0 @@
-Puts "this is a test"
